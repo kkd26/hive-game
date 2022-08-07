b8f0d7a7cc0df186b0a458613d46549fd4771eb1
Krzysztof Druciarek <kkd26@cam.ac.uk>
2022-08-07 20:09:11 +0200

    Initial commit from Create Next App

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hive game</title>
+      </Head>
+      <main>Welcome to the Hive game.</main>
+    </div>
+  );
+}
